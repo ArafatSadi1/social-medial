b1170274e7db9b0aea92e646e411cd68fa58b741
ArafatSadi1 <arafatsadi1@gmail.com>
2022-09-18 21:27:41 +0600

    some update

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Message = () => {
   return (
-    <div className="max-w-[1300px] mx-auto">
+    <div className="max-w-[1300px] h-screen mx-auto">
       <h2 className="text-center text-7xl font-bold text-secondary mt-20">
         Cooming Soon...
       </h2>
